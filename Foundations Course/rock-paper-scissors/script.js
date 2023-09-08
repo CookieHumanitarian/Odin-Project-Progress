d5799b48f415748b112711dcc8f331fe582d7bf9
William Marcus <wmarcus2701@gmail.com>
2023-09-08 19:27:42 +0800

    Added the simulate game functions where program will check win/lose/draw in a game of rock-paper-scissors against the computer

--- Foundations Course/rock-paper-scissors/script.js
+++ Foundations Course/rock-paper-scissors/script.js
@@ -1,8 +1,54 @@
 const selection = ["ROCK", "PAPER", "SCISSORS"];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   let computerChoice = selection[Math.floor(Math.random() * selection.length)];
-  console.log(computerChoice);
+  return computerChoice;
 }
 
-getComputerChoice();
+function simulateGame(playerSelection, computerSelection) {
+  if (playerSelection === "ROCK") {
+    if (computerSelection === "ROCK") {
+      return "Draw!";
+    } else if (computerSelection === "PAPER") {
+      computerScore += 1;
+      return "Computer wins!";
+    } else {
+      playerScore += 1;
+      return "You win!";
+    }
+  } else if (playerSelection === "PAPER") {
+    if (computerSelection === "ROCK") {
+      playerScore += 1;
+      return "You win!!";
+    } else if (computerSelection === "PAPER") {
+      return "Draw!";
+    } else {
+      computerScore += 1;
+      return "Computer wins!";
+    }
+  } else if (playerSelection === "SCISSORS") {
+    if (computerSelection === "ROCK") {
+      computerScore += 1;
+      return "Computer wins!";
+    } else if (computerSelection === "Paper") {
+      playerScore += 1;
+      return "Player wins!";
+    } else {
+      return "Draw!";
+    }
+  } else {
+    return "Invalid Input";
+  }
+}
+
+userInput = prompt("Rock, Paper, or Scissors?");
+userInput = userInput.toUpperCase();
+computerInput = getComputerChoice();
+
+result = simulateGame(userInput, computerInput);
+console.log(result);
+console.log(
+  `Your score: ${playerScore} vs Computer\'s score: ${computerScore}`
+);

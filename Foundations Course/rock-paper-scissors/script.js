5c5c99f344b3e1151f08c5484e6d96544ed0b652
William Marcus <wmarcus2701@gmail.com>
2023-09-18 13:42:56 +0800

    Added event listeners for rock paper and scissors

--- Foundations Course/rock-paper-scissors/script.js
+++ Foundations Course/rock-paper-scissors/script.js
@@ -1,61 +1,58 @@
-function game() {
-  const selection = ["ROCK", "PAPER", "SCISSORS"];
-  let playerScore = 0;
-  let computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-  function getComputerChoice() {
-    let computerChoice =
-      selection[Math.floor(Math.random() * selection.length)];
-    return computerChoice;
-  }
+const rock = document.querySelector("#ROCK");
+rock.addEventListener("click", () => {
+  alert("Hello rock");
+});
 
-  function simulateGame(playerSelection, computerSelection) {
-    if (playerSelection === "ROCK") {
-      if (computerSelection === "ROCK") {
-        return "Draw!";
-      } else if (computerSelection === "PAPER") {
-        computerScore += 1;
-        return "Computer wins!";
-      } else {
-        playerScore += 1;
-        return "You win!";
-      }
-    } else if (playerSelection === "PAPER") {
-      if (computerSelection === "ROCK") {
-        playerScore += 1;
-        return "You win!!";
-      } else if (computerSelection === "PAPER") {
-        return "Draw!";
-      } else {
-        computerScore += 1;
-        return "Computer wins!";
-      }
-    } else if (playerSelection === "SCISSORS") {
-      if (computerSelection === "ROCK") {
-        computerScore += 1;
-        return "Computer wins!";
-      } else if (computerSelection === "Paper") {
-        playerScore += 1;
-        return "Player wins!";
-      } else {
-        return "Draw!";
-      }
-    } else {
-      return "Invalid Input";
-    }
-  }
-  for (let i = 0; i < 5; i++) {
-    userInput = prompt("Rock, Paper, or Scissors?");
-    userInput = userInput.toUpperCase();
-    computerInput = getComputerChoice();
+const paper = document.querySelector("#PAPER");
+paper.addEventListener("click", () => {
+  alert("Hello paper");
+});
 
-    result = simulateGame(userInput, computerInput);
-    console.log(result);
-    console.log(
-      `Your score: ${playerScore} vs Computer\'s score: ${computerScore}`
-    );
-  }
-  console.log("END");
+const scissors = document.querySelector("#SCISSORS");
+scissors.addEventListener("click", () => {
+  alert("Hello scissors");
+});
+
+function getComputerChoice() {
+  let computerChoice = selection[Math.floor(Math.random() * selection.length)];
+  return computerChoice;
 }
 
-game();
+function simulateGame(playerSelection, computerSelection) {
+  if (playerSelection === "ROCK") {
+    if (computerSelection === "ROCK") {
+      return "Draw!";
+    } else if (computerSelection === "PAPER") {
+      computerScore += 1;
+      return "Computer wins!";
+    } else {
+      playerScore += 1;
+      return "You win!";
+    }
+  } else if (playerSelection === "PAPER") {
+    if (computerSelection === "ROCK") {
+      playerScore += 1;
+      return "You win!!";
+    } else if (computerSelection === "PAPER") {
+      return "Draw!";
+    } else {
+      computerScore += 1;
+      return "Computer wins!";
+    }
+  } else if (playerSelection === "SCISSORS") {
+    if (computerSelection === "ROCK") {
+      computerScore += 1;
+      return "Computer wins!";
+    } else if (computerSelection === "Paper") {
+      playerScore += 1;
+      return "Player wins!";
+    } else {
+      return "Draw!";
+    }
+  } else {
+    return "Invalid Input";
+  }
+}

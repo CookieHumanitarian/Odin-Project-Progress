8f06cd48d5040fd8f008877fb44fcb443a9faa13
William Marcus <wmarcus2701@gmail.com>
2023-09-19 18:28:30 +0800

    Added a condition to not let user's custom grid to exceed 100 x 100

--- Foundations Course/etch-a-sketch/script.js
+++ Foundations Course/etch-a-sketch/script.js
@@ -4,7 +4,7 @@ let gridNumber = 10;
 let newSizeButton = document.querySelector("#newSize");
 newSizeButton.addEventListener("click", function () {
   let number = parseInt(prompt("Enter a value", 16));
-  while (isNaN(number) || number < 1) {
+  while (isNaN(number) || number < 1 || number > 100) {
     number = parseInt(prompt("Enter a value", 16));
   }
 

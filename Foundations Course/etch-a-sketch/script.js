0e803dfb941ef269e444340d70f99f4f4585aaf7
William Marcus <wmarcus2701@gmail.com>
2023-09-19 19:06:08 +0800

    Added a prompt that allows user to key in their desired grid size and edited css to make the graph a bit wider

--- Foundations Course/etch-a-sketch/script.js
+++ Foundations Course/etch-a-sketch/script.js
@@ -1,5 +1,5 @@
-const container = document.getElementById("grid");
-let gridNumber = 10;
+const container = document.querySelector("#grid");
+let gridNumber = 16;
 
 let newSizeButton = document.querySelector("#newSize");
 newSizeButton.addEventListener("click", function () {
@@ -7,6 +7,23 @@ newSizeButton.addEventListener("click", function () {
   while (isNaN(number) || number < 1 || number > 100) {
     number = parseInt(prompt("Enter a value", 16));
   }
-
-  createGrid(value);
+  resetGrid();
+  createGrid(number);
 });
+
+function createGrid(value) {
+  let gridSize = value * value;
+  for (let i = 0; i < gridSize; i++) {
+    let grid = document.createElement("div");
+    grid.classList.add("innerGrid");
+    container.style.gridTemplateRows = `repeat(${value}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+    container.appendChild(grid);
+  }
+}
+
+function resetGrid() {
+  container.innerHTML = "";
+}
+
+createGrid(gridNumber);

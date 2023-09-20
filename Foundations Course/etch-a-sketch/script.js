fe45a9925203b7e528255fbc2969ce8b1de1b426
William Marcus <wmarcus2701@gmail.com>
2023-09-20 12:02:40 +0800

    Created an event listener where when a user clicks, it will change the background color to black

--- Foundations Course/etch-a-sketch/script.js
+++ Foundations Course/etch-a-sketch/script.js
@@ -1,6 +1,16 @@
 const container = document.querySelector("#grid");
 let gridNumber = 16;
 
+function defaultColorChange() {
+  let eachGrid = document.querySelectorAll(".innerGrid");
+  console.log(eachGrid);
+  eachGrid.forEach(function (e) {
+    e.addEventListener("click", function () {
+      e.classList.add("black");
+    });
+  });
+}
+
 let newSizeButton = document.querySelector("#newSize");
 newSizeButton.addEventListener("click", function () {
   let number = parseInt(prompt("Enter a value", 16));
@@ -9,12 +19,14 @@ newSizeButton.addEventListener("click", function () {
   }
   resetGrid();
   createGrid(number);
+  defaultColorChange();
 });
 
 let resetButton = document.querySelector("#reset");
 resetButton.addEventListener("click", function () {
   container.innerHTML = "";
   createGrid(gridNumber);
+  defaultColorChange();
 });
 
 function createGrid(value) {
@@ -26,6 +38,7 @@ function createGrid(value) {
     container.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
     container.appendChild(grid);
   }
+  defaultColorChange();
 }
 
 function resetGrid() {

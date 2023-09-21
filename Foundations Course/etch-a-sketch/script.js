03e6663a29ead017c222b3e6716b1109a55a836e
William Marcus <wmarcus2701@gmail.com>
2023-09-21 11:52:41 +0800

    Changed color button to rainbow. Removed click event for element. Will add an eraser function later

--- Foundations Course/etch-a-sketch/script.js
+++ Foundations Course/etch-a-sketch/script.js
@@ -9,18 +9,16 @@ newSizeButton.addEventListener("click", function () {
     number = parseInt(prompt("Enter a value", 16));
   }
   createGrid(number);
-  defaultColorChange();
 });
 
 let resetButton = document.querySelector("#reset");
 resetButton.addEventListener("click", function () {
   createGrid(gridNumber);
-  defaultColorChange();
 });
 
-let colorButton = document.querySelector("#color");
-colorButton.addEventListener("click", function () {
-  return;
+let rainbowButton = document.querySelector("#rainbow");
+rainbowButton.addEventListener("click", function () {
+  rainbowColorChanger();
 });
 
 function createGrid(value) {
@@ -33,6 +31,7 @@ function createGrid(value) {
     container.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
     container.appendChild(grid);
   }
+  defaultColorChange();
 }
 
 function defaultColorChange() {
@@ -44,8 +43,6 @@ function defaultColorChange() {
       if (!e.classList.contains("black")) {
         e.classList.add("black");
         return;
-      } else {
-        e.classList.remove("black");
       }
     });
     e.addEventListener("mouseover", function () {
@@ -59,5 +56,26 @@ function defaultColorChange() {
   });
 }
 
+function rainbowColorChanger() {
+  let eachGrid = document.querySelectorAll(".innerGrid");
+  startDrag = false;
+  eachGrid.forEach(function (e) {
+    e.addEventListener("mousedown", function () {
+      startDrag = true;
+      if (!e.classList.contains("black")) {
+        e.classList.add("black");
+        return;
+      }
+    });
+    e.addEventListener("mouseover", function () {
+      if (!e.classList.contains("black") && startDrag === true) {
+        e.classList.add("black");
+      }
+    });
+    e.addEventListener("mouseup", function () {
+      startDrag = false;
+    });
+  });
+}
 createGrid(gridNumber);
 defaultColorChange();

b52d31d58a9c88393e340afe63f36c2525241dbe
William Marcus <wmarcus2701@gmail.com>
2023-09-21 11:43:06 +0800

    Changed format of code (functions at bottom) and removed reset function and added it to createGrid function to make code neater

--- Foundations Course/etch-a-sketch/script.js
+++ Foundations Course/etch-a-sketch/script.js
@@ -2,6 +2,39 @@ const container = document.querySelector("#grid");
 let gridNumber = 16;
 let startDrag = false;
 
+let newSizeButton = document.querySelector("#newSize");
+newSizeButton.addEventListener("click", function () {
+  let number = parseInt(prompt("Enter a value", 16));
+  while (isNaN(number) || number < 1 || number > 100) {
+    number = parseInt(prompt("Enter a value", 16));
+  }
+  createGrid(number);
+  defaultColorChange();
+});
+
+let resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", function () {
+  createGrid(gridNumber);
+  defaultColorChange();
+});
+
+let colorButton = document.querySelector("#color");
+colorButton.addEventListener("click", function () {
+  return;
+});
+
+function createGrid(value) {
+  container.innerHTML = "";
+  let gridSize = value * value;
+  for (let i = 0; i < gridSize; i++) {
+    let grid = document.createElement("div");
+    grid.classList.add("innerGrid");
+    container.style.gridTemplateRows = `repeat(${value}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+    container.appendChild(grid);
+  }
+}
+
 function defaultColorChange() {
   let eachGrid = document.querySelectorAll(".innerGrid");
   startDrag = false;
@@ -24,39 +57,6 @@ function defaultColorChange() {
       startDrag = false;
     });
   });
-}
-
-let newSizeButton = document.querySelector("#newSize");
-newSizeButton.addEventListener("click", function () {
-  let number = parseInt(prompt("Enter a value", 16));
-  while (isNaN(number) || number < 1 || number > 100) {
-    number = parseInt(prompt("Enter a value", 16));
-  }
-  resetGrid();
-  createGrid(number);
-  defaultColorChange();
-});
-
-let resetButton = document.querySelector("#reset");
-resetButton.addEventListener("click", function () {
-  resetGrid();
-  createGrid(gridNumber);
-  defaultColorChange();
-});
-
-function createGrid(value) {
-  let gridSize = value * value;
-  for (let i = 0; i < gridSize; i++) {
-    let grid = document.createElement("div");
-    grid.classList.add("innerGrid");
-    container.style.gridTemplateRows = `repeat(${value}, 1fr)`;
-    container.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
-    container.appendChild(grid);
-  }
-}
-
-function resetGrid() {
-  container.innerHTML = "";
 }
 
 createGrid(gridNumber);

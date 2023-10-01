f19fad70f21003f7d39266c091f78ef0c5bb9a57
William Marcus <wmarcus2701@gmail.com>
2023-10-01 20:54:51 +0800

    Added a subtraction and addition feature to calculator

--- Foundations Course/calculator/script.js
+++ Foundations Course/calculator/script.js
@@ -2,7 +2,6 @@ let var1 = null;
 let var2 = null;
 let total = 0;
 let sign = "";
-
 let buttons = document.querySelectorAll(".innerButton");
 for (let i = 0; i < buttons.length; i++) {
   let button = buttons[i];
@@ -13,17 +12,28 @@ for (let i = 0; i < buttons.length; i++) {
       var1 = button.innerHTML;
     } else {
       var2 = button.innerHTML;
-      console.log(var2);
     }
 
     if (var1 != null && var2 != null) {
       if (sign == "/") {
+        let temp = var2;
+        document.getElementById("display").innerHTML = temp;
         var1 = divideFunction(var1, var2);
-        document.getElementById("display").innerHTML = var1;
         var2 = null;
       } else if (sign == "*") {
+        let temp = var2;
+        document.getElementById("display").innerHTML = temp;
         var1 = multiplyFunction(var1, var2);
-        document.getElementById("display").innerHTML = var1;
+        var2 = null;
+      } else if (sign == "+") {
+        let temp = var2;
+        document.getElementById("display").innerHTML = temp;
+        var1 = addFunction(var1, var2);
+        var2 = null;
+      } else if (sign == "-") {
+        let temp = var2;
+        document.getElementById("display").innerHTML = temp;
+        var1 = minusFunction(var1, var2);
         var2 = null;
       }
     }
@@ -49,6 +59,16 @@ multiply.addEventListener("click", () => {
   sign = "*";
 });
 
+let addition = document.querySelector(".sign.plus");
+addition.addEventListener("click", () => {
+  sign = "+";
+});
+
+let subtraction = document.querySelector(".sign.minus");
+subtraction.addEventListener("click", () => {
+  sign = "-";
+});
+
 let equal = document.querySelector(".equal");
 equal.addEventListener("click", () => {
   total = var1;
@@ -66,6 +86,18 @@ function multiplyFunction(a, b) {
   let x = Number(a);
   let y = Number(b);
   return x * y;
+}
+
+function addFunction(a, b) {
+  let x = Number(a);
+  let y = Number(b);
+  return x + y;
+}
+
+function minusFunction(a, b) {
+  let x = Number(a);
+  let y = Number(b);
+  return x - y;
 }
 
 function clear() {
